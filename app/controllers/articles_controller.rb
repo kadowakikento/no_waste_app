a85e2b926263daabed110c9cbd2b8a6446f60f16
kento kadowaki <kadowakikento@gmail.com>
2022-08-15 18:20:38 +0900

    deviseのメールを設定

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -22,8 +22,6 @@ class ArticlesController < ApplicationController
   # POST /articles or /articles.json
   def create
     @article = Article.new(article_params)
-
-    
     if @article.save
       redirect_to article_url(@article), notice: "投稿しました" 
     else
@@ -33,24 +31,17 @@ class ArticlesController < ApplicationController
 
   # PATCH/PUT /articles/1 or /articles/1.json
   def update
-    respond_to do |format|
-      if @article.update(article_params)
-        format.html { redirect_to article_url(@article), notice: "更新しました" }
-        format.json { render :show, status: :ok, location: @article }
-      else
-        format.html { render :edit, status: :unprocessable_entity }
-        format.json { render json: @article.errors, status: :unprocessable_entity }
-      end
+    if @article.update(article_params)
+      redirect_to article_url(@article), notice: "更新しました" 
+    else
+      render :edit, status: :unprocessable_entity
     end
   end
 
   # DELETE /articles/1 or /articles/1.json
   def destroy
     @article.destroy
-
-    respond_to do |format|
-      format.html { redirect_to articles_url, notice: "削除しました" }
-      format.json { head :no_content }
+      redirect_to articles_url, notice: "削除しました" 
     end
   end
 
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,11 @@
 Rails.application.routes.draw do
+  # root 'top#index' 
   resources :articles
+
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  end
+
+  if Rails.env.development?
+    mount LetterOpenerWeb::Engine, at: "/letter_opener"
+  end
 end
