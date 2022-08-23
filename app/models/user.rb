4e482f6b123b3efee93ba7fcc230fe8d8cf37131
kento kadowaki <kadowakikento@gmail.com>
2022-08-24 02:18:14 +0900

    :seedling: シードデータ作成

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,8 +1,8 @@
 class User < ApplicationRecord
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
-  has_many :foods
-  has_many :shopping_lists
+  has_many :foods, dependent: :destroy
+  has_many :shopping_lists, dependent: :destroy
 
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,34 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+User.create!([
+  { email: "sample1@sample.com", password: "12345678", admin: true},
+  { email: "sample2@sample.com", password: "12345678" },
+  { email: "sample3@sample.com", password: "12345678" },
+  { email: "sample4@sample.com", password: "12345678" },
+  { email: "sample5@sample.com", password: "12345678" },
+  ])
+
+Food.create!([
+  { name: "キャベツ", due_time: "2022/09/01 17:00", user_id: "1" },
+  { name: "豚肉", due_time: "2022/08/28 13:00", user_id: "2" },
+  { name: "玉ねぎ", due_time: "2022/08/25 19:00", user_id: "3" },
+  { name: "ヨーグルト", due_time: "2022/09/01 10:00", user_id: "4" },
+  { name: "お惣菜", due_time: "2022/08/29 19:00", user_id: "5" },
+])
+
+ShoppingList.create!([
+  { title: "豚肉", content: "小間切れ", user_id: "1" },
+  { title: "キャベツ", content: "高いの", user_id: "2" },
+  { title: "ヨーグルト", content: "無糖", user_id: "3" },
+  { title: "玉ねぎ", content: "5つ入り", user_id: "4" },
+  { title: "お酒", content: "檸檬堂", user_id: "5" },
+])
+
+Comment.create!([
+  { content: "must", shopping_list_id: "1" },
+  { content: "need", shopping_list_id: "2" },
+  { content: "want", shopping_list_id: "3" },
+  { content: "must", shopping_list_id: "4" },
+  { content: "want", shopping_list_id: "5" },
+])
