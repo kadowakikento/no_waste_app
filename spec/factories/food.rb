16cec2a61800590e6bd3b287437408df14eb9c7e
kento kadowaki <kadowakikento@gmail.com>
2022-08-28 13:27:46 +0900

    system/shopping_list_spec.rbのテストを実装

--- spec/factories/food.rb
+++ spec/factories/food.rb
@@ -1,13 +1,11 @@
 FactoryBot.define do
   factory :food do
-    
     name { 'お肉' }
     due_time { "2022-08-29 16:30" }
     association :user
   end
 
   factory :second_food, class: Food do
-    
     name { '野菜' }
     due_time { "2022-08-28 01:30" }
     association :user
